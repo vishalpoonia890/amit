28384788f86a4fe5a4d8da126378ea7803a94488
vishalpoonia890 <codevishaal@gmail.com>
2025-09-13 14:54:19 +0530

    Update TopNav.js

--- frontend/src/components/TopNav.js
+++ frontend/src/components/TopNav.js
@@ -11,14 +11,14 @@ function TopNav({ theme, toggleTheme, financialSummary, unreadCount, onNotificat
                 <button onClick={toggleTheme} className="icon-btn theme-toggle">
                     {theme === 'light' ? (
                         // 🌙 Moon Icon
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M21.75 15.002a9.72 9.72 0 01-3.752.748c-5.385 0-9.75-4.365-9.75-9.75 
                                 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 
                                 12.75 21a9.753 9.753 0 009.002-5.998z" />
                         </svg>
                     ) : (
                         // ☀️ Sun Icon
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" 
                                 d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 
                                 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 
@@ -43,7 +43,7 @@ function TopNav({ theme, toggleTheme, financialSummary, unreadCount, onNotificat
             <div className="nav-right">
                 <button onClick={onNotificationsClick} className="icon-btn notification-btn">
                     {/* 🔔 Bell Icon */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" 
                             d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 
                             8.967 0 0118 9.75V9A6 6 0 006 9v.75a8.967 8.967 0 
