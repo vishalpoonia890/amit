d8c143efe5cc9baead89b6fdaeccee984f66c258
vishalpoonia890 <codevishaal@gmail.com>
2025-09-17 02:01:11 +0530

    Update Icons.js

--- frontend/src/components/Icons.js
+++ frontend/src/components/Icons.js
@@ -44,6 +44,26 @@ export const BitcoinIcon = () => <span>💡</span>;
 export const TwentyDayIcon = () => <span>🛡️</span>;
 export const QuickDisbursalIcon = () => <span>⚡️</span>;
 
+// --- Icons for the "How to Play" section in the Game Lobby ---
+export const SelectGameIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+    </svg>
+);
+
+export const PlaceBetIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V6.375m18 10.5-18-10.5" />
+    </svg>
+);
+
+export const WinBigIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 18.75h-9a9 9 0 119 0zM16.5 18.75a9 9 0 11-18 0h18zM12 12.75a1.5 1.5 0 110-3 1.5 1.5 0 010 3z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v.008" />
+    </svg>
+);
+
 // --- Crypto Icons ---
 export const BtcIcon = () => <svg fill="#F7931A" viewBox="0 0 24 24"><path d="M16.6 13.92c.49-.29.81-.83.81-1.42 0-.92-.75-1.67-1.67-1.67h-1.61v3.33h1.61c.45 0 .87-.17 1.18-.47l-.32-.37zm-1.6-4.17h2.15c.92 0 1.67.75 1.67 1.67s-.75 1.67-1.67 1.67h-2.15V9.75zm3.33 5h-2.15c-.45 0-.87.17-1.18.47l.32.37c.49.29.81.83.81 1.42 0 .92-.75 1.67-1.67 1.67h-2.15v-5h-1.06v5.62c0 .35-.28.63-.63.63H10.5c-.35 0-.63-.28-.63-.63V7.75c0-.35.28-.63.63-.63h3.75c.35 0 .63.28.63.63v1.07h1.61c1.47 0 2.65.96 2.65 2.15 0 .8-.5 1.5-1.21 1.85l1.43 1.67c.2.23.16.58-.07.78-.11.1-.24.15-.37.15-.17 0-.33-.08-.44-.22l-1.4-1.64z"/></svg>;
 export const EthIcon = () => <svg fill="#627EEA" viewBox="0 0 24 24"><path d="M12 1.75l-6.25 10.5 6.25 3.5 6.25-3.5L12 1.75zM12 17.25l-6.25-3.5 6.25 6.25 6.25-6.25-6.25 3.5z"/></svg>;
